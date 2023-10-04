3623d01fc912b4c88ef53cfd10fdba8f5e6a49e0
Gustavo Souza <gustavo.araujo@aluno.uepa.br>
2023-10-04 16:41:18 -0300

    fixed script

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 #! /usr/bin/env node
 import fs from "fs/promises";
-import { get_json_series } from "./utils/index.js";
+import { get_json_series, get_series_per_page } from "./utils/index.js";
 import { program } from "commander";
 import downloader from "nodejs-file-downloader";
 import cliProgress from "cli-progress";
 
 import colors from "ansi-colors";
 
-const bar = new cliProgress.SingleBar({
+const chapter_bar = new cliProgress.SingleBar({
   format:
     "Download progress |" +
     colors.cyan("{bar}") +
     "| {percentage}% || {value}/{total} chapters",
+  barCompleteChar: "\u2588",
+  barIncompleteChar: "\u2591",
+  hideCursor: true,
+});
+
+const series_bar = new cliProgress.SingleBar({
+  format:
+    "Download progress |" +
+    colors.cyan("{bar}") +
+    "| {percentage}% || {value}/{total} series",
   barCompleteChar: "\u2588",
   barIncompleteChar: "\u2591",
   hideCursor: true,
@@ -22,19 +32,22 @@ program
   .description("CLI to download series from yugen")
   .version("1.0.0");
 
-program.argument("<slug>", "slug of the series to be downloaded");
+program.argument(
+  "<page>",
+  "page of the query of series to be downloaded (1000 series per page)"
+);
 program.parse();
 
-const slug = program.args[0];
+const page = program.args[0];
 
-async function download() {
+async function download(slug) {
   const json = await get_json_series(slug);
   await fs.mkdir(json.title);
   console.log("Number of chapters to be downloaded: " + json.chapters.length);
-  bar.start(json.chapters.length, 0, {});
+  chapter_bar.start(json.chapters.length, 0, {});
   for (let i = 0; i <= json.chapters.length - 1; i++) {
     const chapter = json.chapters[i];
-    bar.update(i + 1);
+    chapter_bar.update(i + 1);
     const images = chapter.chapter_data.images;
     const images_array = images.map(
       (image, index) =>
@@ -50,7 +63,22 @@ async function download() {
     );
     await Promise.all(images_array.map((item) => item.download()));
   }
-  bar.stop();
+  chapter_bar.stop();
 }
 
-download();
+async function init() {
+  const json = await get_series_per_page(page);
+  console.log("Number of series to be downloaded: " + json.length);
+  series_bar.start(json.length, 0, {});
+  for (let i = 0; i <= json.length - 1; i++) {
+    try {
+      series_bar.update(i + 1);
+      await download(json[i]);
+    } catch (error) {
+      console.log(`Series ${json[i]} wasnt able to be downloaded`);
+    }
+  }
+  series_bar.stop();
+}
+
+init();
--- utils/index.js
+++ utils/index.js
@@ -4,3 +4,8 @@ export async function get_json_series(slug) {
   const json = await API.get("/series/json/" + slug);
   return json.data;
 }
+
+export async function get_series_per_page(page) {
+  const json = await API.get("/query?perPage=1000&page=" + page);
+  return json.data.data.map((series) => series.series_slug);
+}
